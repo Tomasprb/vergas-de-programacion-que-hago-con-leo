38e4c3feb50069e7313dc9af23a562552dd284d4
Android Studio <devandroid@ort>
2019-06-11 11:55:44 -0300

    falcon verde

---
+++ RIPJoseYRigoberto/app/src/main/java/com/example/ripjoseyrigoberto/actividad_principal.java
@@ -0,0 +1,18 @@
+package com.example.ripjoseyrigoberto;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class actividad_principal extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actividad_principal);
+
+        Intent Mapa;
+        Mapa= new Intent(this, Mapa.class);
+        startActivity(Mapa);
+    }
+}
