2bfd6fe406310515962c755c86f4ce1083ddb981
Android Studio <devandroid@ort>
2019-06-13 10:18:02 -0300

    que hice para merecerme esto??????????

--- RIPJoseYRigoberto/app/src/main/java/com/example/ripjoseyrigoberto/FragmentBuscarCategoria.java
+++ RIPJoseYRigoberto/app/src/main/java/com/example/ripjoseyrigoberto/FragmentBuscarCategoria.java
@@ -4,6 +4,7 @@ package com.example.ripjoseyrigoberto;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.JsonReader;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
@@ -52,17 +55,59 @@ public class FragmentBuscarCategoria extends Fragment implements View.OnClickLis
                 URL miRuta = new URL("http://epok.buenosaires.gob.ar/getCategorias");
                 HttpURLConnection miConexion = (HttpURLConnection) miRuta.openConnection();
                 if (miConexion.getResponseCode()==200){
-                    Log.d("algoMuyOriginal","Me conecto");
-                    
+                    Log.d("algoMuyOriginal","Terapeuta: el JSON no existe, no puede hacerte daño. JSON: iniciando conexion");
+                    InputStream cuerpoRespuesta = miConexion.getInputStream();
+                    InputStreamReader lectorRespuesta = new InputStreamReader(cuerpoRespuesta,"UTF-8");
+                    procesarJSONLeido(lectorRespuesta);
+                }else {
+                    Log.d("algoMuyOriginal"," error en la conexion");
                 }
             }
             catch(Exception error){
             Log.d("AccesoAPI","Hubo un error al conectarme " + error.getMessage());
-        }
+            }
             return null;
 
         }
     }
+
+    public void procesarJSONLeido(InputStreamReader streamLeido){
+        JsonReader JSONLeido = new JsonReader(streamLeido);
+        try {
+            JSONLeido.beginObject();
+            while (JSONLeido.hasNext()){
+                String nombreElementoActual=JSONLeido.nextName();
+                Log.d("algoMuyOriginal","un trauma salvaje ha aparecido");
+                if (nombreElementoActual.equals("cantidad_de_categorias")){
+                    int cantidadCategorias=JSONLeido.nextInt();
+                    Log.d("algoMuyOriginal","la cantidad de traumas salvajes es: " + cantidadCategorias);
+                }else {
+                    JSONLeido.beginArray();
+                    Log.d("algoMuyOriginal","array");
+                    while (JSONLeido.hasNext()){
+                        JSONLeido.beginObject();
+                        Log.d("algoMuyOriginal","object");
+                        while (JSONLeido.hasNext()){
+                            nombreElementoActual = JSONLeido.nextName();
+                            if (nombreElementoActual.equals("nombre_normalizado")){
+                                String valorElementoActual = JSONLeido.nextString();
+                                Log.d("algoMuyOriginal","Terapeuta: ¿que viste?¿que paso? Yo: " + valorElementoActual);
+                                datosLista.add(valorElementoActual);
+                            } else {
+                                JSONLeido.skipValue();
+                            }
+                        }
+                        JSONLeido.endObject();
+                    }
+                    JSONLeido.endArray();
+                }
+            }
+            JSONLeido.endObject();
+        }catch(Exception error){
+            Log.d("AccesoAPI","Hubo un error al conectarme " + error.getMessage());
+        }
+    }
+
     public void onClick(View vistaRecibida){
         Log.d("algoMuyOriginal", "fuera de mi patio niños apestosos");
 
